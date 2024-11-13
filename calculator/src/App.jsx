e2ab0ab7ba0f28fdb02b21f893f5fbbeddf7b1e2
magraina <marinafcegner@gmail.com>
2024-11-13 14:30:05 +0100

    feat: add preview of result to calculator app

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -4,32 +4,58 @@ import CalculatorButton from './components/CalculatorButton';
 
 function App() {
 	const [calculation, setCalculation] = useState('0');
+	const [possibleResult, setPossibleResult] = useState('');
+
+	const updatePossibleResult = (value) => {
+		setPossibleResult((previousResult) => {
+			try {
+				const result = eval(value);
+
+				if(result === 0 || result === parseInt(value)) {
+					return '';
+				}
+
+				return result;
+			} catch {
+				return previousResult;
+			}
+		});
+	};
 
 	const updateCalculation = (value) => {
 		setCalculation((prevCalculation) => {
-			if(prevCalculation === '0') {
+			updatePossibleResult(prevCalculation + value);
+			if(prevCalculation === '0' || prevCalculation === 'Error') {
 				return value;
 			}
+
 			return prevCalculation + value;
 		});
 	};
 
 	const resetCalculation = () => {
 		setCalculation('0');
+		setPossibleResult('');
 	};
 
 	const calculateResult = () => {
 		setCalculation((prevCalculation) => {
+			if(isNaN(parseInt(prevCalculation))) return 'Error';
 			try {
 				return eval(prevCalculation);
 			} catch (error) {
 				return 'Error';
 			}
 		});
+		setPossibleResult('');
 	}
 
 	const removeLastCharacter = () => {
 		setCalculation((prevCalculation) => {
+			updatePossibleResult(prevCalculation + value);
+			if(prevCalculation === 'Error') {
+				return 'Error';
+			}
 			if(prevCalculation.length === 1) {
 				return '0';
 			}
@@ -40,7 +66,8 @@ function App() {
 	return (
 		<>
 			<h1>Calculator</h1>
-			<h1>{calculation}</h1>
+			<h2>{calculation}</h2>
+			<p className='possible-result'>{possibleResult}</p>
 			<div className="calculator-button-field">
 				<CalculatorButton value="AC" colored onClick={resetCalculation} />
 				<CalculatorButton value="" onClick={()=>{}} />
